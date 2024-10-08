bc11eeeff082cc5620b50ba8ebeef073890c5105
Erick007-tech <monteiroerick207@gmail.com>
2024-10-08 10:53:42 -0300

    add Jorge hungry and beauty file
    depending on the beauty and hunger variable, Jorge's aspects will change

---
+++ #first.py
@@ -0,0 +1,23 @@
+#first
+beleza=0
+fome=0
+
+if beleza<=0:
+  print("jorge é feio")
+elif beleza<=1 or beleza>=9:
+  print("jorge é bonitinho")
+elif beleza>=10:
+  print("jorge é bonito")
+
+if fome<=0:
+  print("jorge está cheio")
+elif fome<=1 or fome<=9:
+  print("jorge com fominha")
+elif fome>=10:
+  print("jorge esta faminto")
+
+
+print( )
+
+print(fome)
+
