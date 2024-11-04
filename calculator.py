94117a4d95baeb83ca5ad6dd041495db0e6dafb2
Erick007-tech <monteiroerick207@gmail.com>
2024-11-04 12:19:47 -0300

    Update calculator.py
    mudança de uma incoerência do final

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@
 x=float(input("digite o primeiro numer: ").replace(",", ".")) 
 
 #possibilita a escolha de operação
-escolha=input("Escolha a operacão dessas operações: x, :, -, +, ^ ||| ")
+escolha=input("Escolha a operacão dessas operações: x, :, -, +, * ||| ")
 
 #escolher segundo número
 y=float(input("digite o segundo numero: ").replace(",", "."))
@@ -16,7 +16,7 @@ elif escolha=="+":
   resultado=x+y
 elif escolha=="-":
   resultado=x-y
-elif escolha=="^":
+elif escolha=="*":
   resultado=x**y
 
 resultado=f"{resultado:_.2f}"
@@ -25,20 +25,20 @@ resultado=resultado.replace(".", ",").replace("_", ".") #As linhas 22 e 23 fazem
 resultado_str=resultado[-3:]#os codigos desta linha até a 28 servem para retirar o ",00" do final se o numero for inteiro
 if resultado_str==",00":
   resultado_str_cplt=str(resultado)
-  resultado=int(resultado_str_cplt.replace(resultado_str, " "))
+  resultado=resultado_str_cplt.replace(resultado_str, " ")
 
 x=f"{x:_.2f}" #os codigos desta linha até a 35 servem para retirar o ",00" do final se o numero for inteiro
 x=x.replace(".", ",").replace("_", ".")
 x_str=x[-3:]
 if x_str==",00":
   x_str_cplt=str(x)
-  x=int(x_str_cplt.replace(x_str, " "))
+  x=x_str_cplt.replace(x_str, " ")
 
 y=f"{y:_.2f}" #os codigos desta linha até a 42 servem para retirar o ",00" do final se o numero for inteiro
 y=y.replace(".", ",").replace("_", ".")
 y_str=y[-3:]
 if y_str==",00":
   y_str_cplt=str(y)
-  y=int(y_str_cplt.replace(y_str, " "))
+  y=y_str_cplt.replace(y_str, " ")
 
 print(f"{x} {escolha} {y} = {resultado}") #Mostra os números escolhidos a operação e o resultado final
